e3c1c9763d993fe34128743227c6bb58ec51de13
Pantheum <67032593+Pantheum@users.noreply.github.com>
2022-08-07 11:42:11 -0600

    010
    010

--- assets/js/SCRIPTS.js
+++ assets/js/SCRIPTS.js
@@ -1,7 +1,2 @@
-var audio = new Audio("/assets/music/Cmaj.mp3");
-var iframe = document.getElementById("dodeca");
-
-iframe.onclick = function() {
-  audio.play();
-  console.log("test");
-}
+var sample = document.getElementById("audio");
+sample.play();
