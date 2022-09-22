b1f70d36fa8bf6daf2961c372fc711ec4c5e25e7
Pantheum <67032593+Pantheum@users.noreply.github.com>
2022-09-22 16:34:25 -0600

    044
    044

--- assets/js/SCRIPTS.js
+++ assets/js/SCRIPTS.js
@@ -12,84 +12,54 @@ function toggleMobileMenu(menu) {
 
 function loadimg(toload) {
 
-    if (toload == 00) {
-        screen.src = "images/assets/CRT_INTRO.png";
-        bttn.src = "images/icons/fab_off.png"
-        bttnstate = 0
+    if (toload == 00) { 
+        screen.src = "images/assets/crt_intro.png";
+
     };
 
     if (toload == 01) {
         screen.src = "images/assets/crt_01.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 02) {
         screen.src = "images/assets/crt_02.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 03) {
         screen.src = "images/assets/crt_03.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 04) {
         screen.src = "images/assets/crt_04.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 05) {
         screen.src = "images/assets/crt_05.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 06) {
         screen.src = "images/assets/crt_06.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 07) {
         screen.src = "images/assets/crt_07.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 08) {
         screen.src = "images/assets/crt_08.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 09) {
         screen.src = "images/assets/crt_09.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 10) {
         screen.src = "images/assets/crt_10.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
     if (toload == 11) {
         screen.src = "images/assets/crt_11.png";
-        bttn.src = "images/icons/fab_on.png"
-        bttnstate = 1
     };
 
-    if (toload == "buttn" && bttnstate == 1) {
-        screen.src = "images/assets/CRT_INST.png"
-        bttn.src = "images/icons/fab_off.png"
-        bttnstate = 0;
-    }
 
-
-
-}
+};
